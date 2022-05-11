f6d0fef3eec4e50af91d581d2b26b530ef7c8424
Jianwen Xu <stephy.jxu@gmail.com>
2022-05-10 19:04:28 -0500

    Updated function to make calculation chainable

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ function operate(a, b, str) {
             return Number(a) * Number(b);
             break;
         case '/':
-            return Number(a) / Number(b);
+            if(b === '0') return 'Infinity'
+            else return Number(a) / Number(b);
     }
 }
 
@@ -32,11 +33,17 @@ let lastEntry = [];//store individual numbers and all operators
 
 const numbers = Array.from(document.querySelectorAll('.number'));
 const operators = Array.from(document.querySelectorAll('.operator'));
+const equal = document.querySelector('#equal');
+const clear = document.querySelector('#clear');
+const backspace = document.querySelector('#backspace');
 
 numbers.forEach(number => number.addEventListener('click', storeToTemp));
 numbers.forEach(number => number.addEventListener('click', showNumber));
 
 operators.forEach(operator => operator.addEventListener('click', calcOp));
+equal.addEventListener('click', calcEqual);
+clear.addEventListener('click', clearAll);
+backspace.addEventListener('click', deleteLast);
 
 
 function showNumber() {
@@ -71,7 +78,9 @@ function isLastEntryOp(arr) {
     return '+-*/'.includes(arr[arr.length -2]);
 }
 
-
+function isLastEntryNum(arr) {
+    return '0123456789.'.includes(arr[arr.length -1]);
+}
 //if an operator is pressed, the following happens
 
 
@@ -81,29 +90,89 @@ function calcOp(e) {
     storeOperator(e);
 
 // check if this is the first operator, if yes, return to get another number; 
-    if (storedValues.length <= 1) return;
+    if (storedValues.length < 2) return;
 
-//if no, check if it has higher precedence than the last;
-
-    //When calculate from left to right
-    else if (precedence[`${storedOperators[storedOperators.length - 1]}`] <= precedence[`${storedOperators[storedOperators.length - 2]}`]) {
+    else if (storedValues.length == 2) {
+       if (precedence[storedOperators[1]] <= precedence[storedOperators[0]]) {
         calcParas = storedValues.splice(0, 2);
         calcParas.push(storedOperators.shift());
         result = operate(...calcParas);
         storedValues.splice(0, 0, result);
         display.innerHTML = result;
         calcParas = [];
+       }
+       else return;
     }
-    //When calculate from right to left
-    else if (precedence[e.target.innerHTML] > precedence[`${storedOperators[storedOperators.length - 2]}`]) {
-        if (storedValues.length < 3) return;
-        else if (storedValues.length === 3) {
-            calcParas.push(storedValues.splice(1, 2));
-            calcParas.push(storedOperators.pop());
+    //When calculate from left to right
+    else if (storedValues.length == 3) {
+        if (precedence[storedOperators[2]] == precedence[storedOperators[1]]) {
+            calcParas = storedValues.splice(1, 2);
+            calcParas.push(storedOperators[1]);
+            storedOperators.splice(1, 1)
             result = operate(...calcParas);
             storedValues.push(result);
+            display.innerHTML = result;
+            calcParas = [];    
+        }
+        else if (precedence[storedOperators[2]] < precedence[storedOperators[1]]) {
+            calcParas = storedValues.splice(1, 2);
+            calcParas.push(storedOperators[1]);
+            storedOperators.splice(1, 1);
+            result = operate(...calcParas);
+            storedValues.push(result);
+            calcParas = storedValues.splice(0, 2);
+            calcParas.push(storedOperators.shift());
+            result = operate(...calcParas);
+            storedValues.unshift(result);
             display.innerHTML = result;
             calcParas = [];
         }
     }
+}
+
+function calcEqual() {
+    storeToPerm();
+    if (!isLastEntryNum(lastEntry) || storedValues.length < 2) {
+        display.innerHTML = 'Error';
+        return;
+    }
+    else if (storedValues.length == 2) {
+         calcParas = storedValues.splice(0, 2);
+         calcParas.push(storedOperators.pop());
+         result = operate(...calcParas);
+         display.innerHTML = result;
+         calcParas = [];  
+     }
+    else if (storedValues.length == 3) {
+        calcParas = storedValues.splice(1, 2);
+        calcParas.push(storedOperators[1]);
+        storedOperators.splice(1, 1);
+        result = operate(...calcParas);
+        storedValues.push(result);
+        calcParas = storedValues.splice(0, 2);
+        calcParas.push(storedOperators.shift());
+        result = operate(...calcParas);
+        display.innerHTML = result;
+        calcParas = [];
+    }
+    lastEntry = [];
+}
+
+function clearAll() {
+    storedValues = []; 
+    storedOperators = [];
+    tempArray = []; 
+    tempNumber = '0';
+    result = 0;
+    calcParas = [];
+    lastEntry = [];
+    display.innerHTML = '0';
+}
+
+function deleteLast() {
+    tempArray.splice(-1, 1);
+    lastEntry.splice(-1, 1);
+    tempNumber = tempArray.join('');
+    if (tempArray.length === 0) tempNumber = '0';
+    display.innerHTML = tempNumber;
 }
